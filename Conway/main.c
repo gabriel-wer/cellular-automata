83f3a47575ccae2f8af958ffb74c6556b25cecf1
gabriel-wer <gabrielflpa@gmail.com>
2023-02-23 23:04:33 -0300

    Working build + C11 make

--- Conway/main.c
+++ Conway/main.c
@@ -18,16 +18,18 @@
 
 int **generate_map(){
     int **map;
-    map = malloc(sizeof(int *) * ROWS);
+    map = malloc(sizeof(int *) * COLUMNS);
     srand(time(0));
-    for (int i=0; i < ROWS; i++){
-        map[i] = malloc(sizeof(int *) * COLUMNS);
-        for (int j=0; j < COLUMNS; j++){ map[i][j] = rand() % 2; }
+    for (int i=0; i < COLUMNS; i++){
+        map[i] = malloc(sizeof(int) * ROWS);
+        for (int j=0; j < ROWS; j++){ 
+            map[i][j] = rand() % 2; 
+        }
     }
     return map;
 }
 
-int detect_collision(int** board, int i, int j){
+void detect_collision(int **board, int i, int j){
     int count = 0;
     for (int x=i-1; x<=i+1; x++){
         for (int y=j-1; y<=j+1; y++){
@@ -39,31 +41,13 @@ int detect_collision(int** board, int i, int j){
             if (x == i && y == j) { continue; }
 
             //count neighbours
-            if (board[x][y] == 1) { count += 1; }
-        }
-    }
-    return count;
-}
-
-void calculate(int** board, SDL_Renderer* renderer){
-    for (int j=0; j < ROWS; j++){
-        for (int i=0; i < COLUMNS; i++){
-            int neighbours = 0;
-            neighbours = detect_collision(board, i, j);
-            if (neighbours == 3) { board[i][j] = 1; }
-            if (neighbours > 3 || neighbours < 2) { board[i][j] = 0; }
-
-            if (board[i][j] == 1){
-                SDL_Rect r; 
-                r.x = j*CELLSIZE;
-                r.y = i*CELLSIZE;
-                r.w = CELLSIZE-1;
-                r.h = CELLSIZE-1;
-                SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-                SDL_RenderFillRect(renderer, &r);
+            if (board[x][y] == 1) { 
+                count += 1; 
             }
         }
     }
+    if (count == 3) { board[i][j] = 1; }
+    if (count > 3 || count < 2) { board[i][j] = 0; }
 }
 
 int main(void){
@@ -74,15 +58,27 @@ int main(void){
     SDL_Renderer* renderer = NULL;
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    int **board;
-    board = generate_map();
+    int **board = generate_map();
 
     SDL_Event event;
     bool running = true;
     while (running){
-        unsigned int start = SDL_GetPerformanceCounter();
+        float start = SDL_GetPerformanceCounter();
         SDL_RenderClear(renderer);
-        calculate(board, renderer);
+        for (int j=0; j < ROWS; j++){
+            for (int i=0; i < COLUMNS; i++){
+                detect_collision(board, i, j);
+                if (board[i][j] == 1){
+                    SDL_Rect r; 
+                    r.x = j*CELLSIZE;
+                    r.y = i*CELLSIZE;
+                    r.w = CELLSIZE-1;
+                    r.h = CELLSIZE-1;
+                    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+                    SDL_RenderFillRect(renderer, &r);
+                }
+            }
+        }
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderPresent(renderer);
 
@@ -98,11 +94,10 @@ int main(void){
                     break;
             }
         }
-        unsigned int end = SDL_GetPerformanceCounter();
-        unsigned int elapsedMS = (end - start) / (float)SDL_GetPerformanceFrequency() * 1000.0f;
+        float end = SDL_GetPerformanceCounter();
+        float elapsedMS = (end - start) / (float)SDL_GetPerformanceFrequency() * 1000.0f;
         SDL_Delay(floor(16.666f - elapsedMS));
     }
-    free(board);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
